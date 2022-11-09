e914a4a13b20051bfe3939d98f8ee8b70a86e4a9
moddas311 <moddasir311@gmail.com>
2022-11-09 20:11:44 +0600

    single data modified with id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -32,6 +32,12 @@ async function run() {
             const allServices = await cursor.toArray();
             res.send(allServices);
         });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCOllection.findOne(query);
+            res.send(service);
+        })
     }
     finally {
 
