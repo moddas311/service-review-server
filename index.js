75331ea795155a869b58c042c843824f0ab949c9
moddas311 <moddasir311@gmail.com>
2022-11-09 08:59:24 +0600

    mongodb basic setup done

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// username = reviewDb
+// pass = 8BUqSSRNxpHgRPNk
 
 
 // Middle wares 
 app.use(cors());
 app.use(express.json())
 
+console.log(process.env.DB_REVIEW);
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.w89pmsb.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
+
 app.get('/', (req, res) => {
-    res.send('Service Review Assignment Running');
+    res.send('Service Review Assignment Server Is Running');
 });
 
 app.listen(port, () => {
