3f50c13621269443e13c52d95d7ae9a1aa99bf8a
moddas311 <moddasir311@gmail.com>
2022-11-09 11:29:52 +0600

    service added with limit and also added allservices

--- index.js
+++ index.js
@@ -6,25 +6,38 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-// username = reviewDb
-// pass = 8BUqSSRNxpHgRPNk
 
 
 // Middle wares 
 app.use(cors());
 app.use(express.json())
 
-console.log(process.env.DB_REVIEW);
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.w89pmsb.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_REVIEW}:${process.env.DB_PASSWORD}@cluster0.w89pmsb.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const serviceCOllection = client.db('photography').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCOllection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        app.get('/allservice', async (req, res) => {
+            const query = {};
+            const cursor = serviceCOllection.find(query);
+            const allServices = await cursor.toArray();
+            res.send(allServices);
+        });
+    }
+    finally {
 
+    }
+}
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.send('Service Review Assignment Server Is Running');
