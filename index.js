fde70ac1fcdd9f03a1fc66e1e9d704448b9ddcec
moddas311 <moddasir311@gmail.com>
2022-11-10 03:44:16 +0600

    post api added

--- index.js
+++ index.js
@@ -41,6 +41,19 @@ async function run() {
         });
 
         // review api
+
+        app.get('/reviews', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
